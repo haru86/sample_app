9411fcfa8aa131e32d90f0061aa777845c410bd1
haru <your.email@example.com>
2015-03-01 18:53:00 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
  	end
 
 	def new
@@ -62,12 +63,12 @@ class UsersController < ApplicationController
     	#  redirect_to signin_url, notice: "Please sign in." unless signed_in?
     	#end
 
-		def signed_in_user
-     	unless signed_in?
-        		store_location
-        		redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+		#def signed_in_user
+    # 	unless signed_in?
+    #    		store_location
+    #    		redirect_to signin_url, notice: "Please sign in."
+    #  end
+    #end
 
     def correct_user
       @user = User.find(params[:id])
